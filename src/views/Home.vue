127ed944e70a21d16812f0f99d029d705f9a0667
Ruby <iruby361@gmail.com>
2021-07-28 21:18:30 +0700

    upload file vue

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+       <!-- Page Preloder -->
+    <!-- <div id="preloder">
+        <div class="loader"></div>
+    </div> untuk loader -->
+    <Header/>
+    <Carousel/>
+    <Banner/>
+    <Instagram/>
+    <Partner/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Carousel from '@/components/Carousel.vue'
+import Banner from '@/components/Banner.vue'
+import Instagram from '@/components/Instagram.vue'
+import Partner from '@/components/Partner.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    Header,
+    Carousel,
+    Banner,
+    Instagram,
+    Partner,
+    Footer,
+  }
+}
+</script>
